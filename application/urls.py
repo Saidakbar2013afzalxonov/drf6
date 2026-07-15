45454f18b1dbdbbca3735d8aba9c212d6054b00d
DostoevskiyRiuui <Hobbycool9@gmail.com>
2026-07-15 13:41:36 +0500

    CRUD

--- application/urls.py
+++ application/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import EmployeeViewSet
+from . import views
 
 urlpatterns = [
-    path('', EmployeeViewSet.as_view({'get': 'list'})),
+    path("employees/", views.EmployeeListAPIView.as_view()),
+    path("employee/<int:pk>/", views.EmployeeRetrieveAPIView.as_view()),
+    path("employee/create/", views.EmployeeCreateAPIView.as_view()),
+    path("employee/update/<int:pk>/", views.EmployeeUpdateAPIView.as_view()),
+    path("employee/delete/<int:pk>/", views.EmployeeDestroyAPIView.as_view()),
+
+    path("employees2/", views.EmployeeListCreateAPIView.as_view()),
+    path("employee/ru/<int:pk>/", views.EmployeeRetrieveUpdateAPIView.as_view()),
+    path("employee/rd/<int:pk>/", views.EmployeeRetrieveDestroyAPIView.as_view()),
+    path("employee/rud/<int:pk>/", views.EmployeeRetrieveUpdateDestroyAPIView.as_view()),
 ]
--- application/views.py
+++ application/views.py
@@ -2,7 +2,57 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Employee
 from .serializers import EmployeeSerializer
+from rest_framework.generics import ListAPIView,RetrieveAPIView,CreateAPIView,UpdateAPIView,DestroyAPIView,ListCreateAPIView,RetrieveUpdateAPIView,RetrieveDestroyAPIView,RetrieveUpdateDestroyAPIView
+
+
 # Create your views here.
-class EmployeeViewSet(viewsets.ModelViewSet):
+
+class EmployeeListAPIView(ListAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+
+class EmployeeRetrieveAPIView(RetrieveAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+
+class EmployeeCreateAPIView(CreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+
+class EmployeeUpdateAPIView(UpdateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+
+class EmployeeDestroyAPIView(DestroyAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+
+class EmployeeListCreateAPIView(ListCreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class EmployeeRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class EmployeeRetrieveDestroyAPIView(RetrieveDestroyAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+
+class EmployeeRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
